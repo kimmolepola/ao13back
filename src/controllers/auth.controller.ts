1d08aa73d4dd01b516cd346be7fcebc6f42081cc
Kimmo Lepola <kimmo.lepola@gmail.com>
2024-04-05 22:06:07 +0300

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -33,6 +33,7 @@ export const turnCredentialsController = async (
 };
 
 export const loginController = async (req: any, res: any, next: any) => {
+  console.log("--login req:", req);
   const loginService = await login(req.body);
   return res.json(loginService);
 };
